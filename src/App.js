b8c56acf36f5aef8b3fc526b42f14c9cee004e06
crudd12 <crudd12@gmail.com>
2024-04-08 12:57:04 -0700

    added components and context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import SearchBar from "./Components/SearchBar"
+import Gallery from "./Components/Gallery"
+import { DataContext } from "./context/DataContext"
+import { SearchContext } from "./context/SearchContext";
 
 function App() {
+  const [message, setMessage] = useState('Search for Music!')
+  const [data, setData] = useState([])
+  let searchInput = useRef('')
+
+  const handleSearch = (e, term) => {
+    // stops the page from refeshing on submit click
+    e.preventDefault()
+    const fetchData = async () => {
+      const url = encodeURI(`https://itunes.apple.com/search?term=${term}`)
+      document.title = `${term} Music`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+
+      if (data.results.length) {
+        setData(data.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    if (term)
+      fetchData()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
+        <SearchBar handleSearch={handleSearch} />
+      </SearchContext.Provider>
+      {message}
+
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
